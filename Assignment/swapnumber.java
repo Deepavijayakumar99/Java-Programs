7a613097d734230ddb55b494f4a2216638aebfcd
Deepavijayakumar99 <103984788+Deepavijayakumar99@users.noreply.github.com>
2022-04-19 21:49:50 -0700

    Add files via upload

---
+++ Assignment/swapnumber.java
@@ -0,0 +1,16 @@
+class Swapnumber
+{
+public static void main(String args[])
+{
+int firstno=10,secondno=20,temp;
+System.out.println("before Swapping");
+System.out.println("the value of first number:"+firstno);
+System.out.println("the value of second number:"+secondno);
+temp=firstno;
+firstno=secondno;
+secondno=temp;
+System.out.println("after Swapping");
+System.out.println("the value of first number:"+firstno);
+System.out.println("the value of second number:"+secondno);
+}
+}
