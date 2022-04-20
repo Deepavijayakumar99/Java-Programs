ef80bb448cbc497ad057b05a6f5f53296413c891
Deepavijayakumar99 <103984788+Deepavijayakumar99@users.noreply.github.com>
2022-04-19 21:48:20 -0700

    Add files via upload

---
+++ Assignment/Addnumber.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Addnumber
+{
+public class void main(String args[])
+{
+System.out.println("sum of two numbers");
+int number1=20;
+int number2=30;
+int sum=number1+number2;
+System.out.println("sum of two numbers(number1+number2)");
+}
+}
