88f9ebf792e41b582fa839182dada252bb5eb3fc
Deepavijayakumar99 <103984788+Deepavijayakumar99@users.noreply.github.com>
2022-04-21 13:54:56 +0530

    Add files via upload

---
+++ Dailyactivity/relation.java
@@ -0,0 +1,13 @@
+class relation
+{
+public static void main(String args[])
+{
+int a=20, b=30;
+System.out.println(a>b);
+System.out.println(a<b);
+System.out.println(a>=b);
+System.out.println(a<=b);
+System.out.println(a!=b);
+System.out.println(a==b);
+}
+}
