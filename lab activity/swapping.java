dbe0340fc8de20dc0ce68cc18af2fdcd9477ad95
Deepavijayakumar99 <103984788+Deepavijayakumar99@users.noreply.github.com>
2022-04-21 13:08:37 +0530

    Add files via upload

---
+++ lab activity/swapping.java
@@ -0,0 +1,16 @@
+class swapping{
+public static void main(String args[])
+{
+int x,y,t;
+System.out.println("Enter the value of x and y");
+x=5;
+y=6;
+t=?
+System.out.println("before swapping the values");
+
+t=x;
+x=y;
+y=t;
+System.out.println("after swapping the values");
+}
+}
