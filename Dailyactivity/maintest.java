319cad4a201e9a950a0f62af3d68b1bbfb44a531
Deepavijayakumar99 <103984788+Deepavijayakumar99@users.noreply.github.com>
2022-04-25 12:11:39 +0530

    Add files via upload

---
+++ Dailyactivity/maintest.java
@@ -0,0 +1,19 @@
+class testclass
+{
+int rollno;
+String name,location;
+}
+class maintest
+{
+public static void main(String args[])
+{
+testclass t=new testclass();
+t.rollno=4;
+t.name="preetha";
+t.location="chennai";
+System.out.println(t.rollno);
+System.out.println(t.name);
+System.out.println(t.location);
+System.out.println();
+}
+}
