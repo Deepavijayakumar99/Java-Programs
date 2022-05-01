14899637f6d922e76eaef32a60e4f9afc3c83d27
Deepavijayakumar99 <103984788+Deepavijayakumar99@users.noreply.github.com>
2022-05-01 14:54:50 +0530

    Add files via upload

---
+++ Dailyactivity/fibonaccicount.java
@@ -0,0 +1,17 @@
+class fibonaccicount
+{
+public static void main(String abc[])
+{
+int a=0;
+int b=1;
+int c=0;
+int d=10;
+for(int i=0;i<10;i++)
+{
+System.out.println(c);
+c=a+b;
+a=b;
+b=c;
+}
+}
+}
