0de0b28db6ae62ec17196758a39229ae527e0552
Deepavijayakumar99 <103984788+Deepavijayakumar99@users.noreply.github.com>
2022-05-01 14:52:08 +0530

    Add files via upload

---
+++ lab activity/stringreverse.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class stringreverse
+{
+public static void main(String args[])
+{
+String s="welcome";
+String s1[]=s.split("");
+ String s2="";
+   
+for(int i=s1.length-1;i>=0;i--){
+System.out.print(s1[i]+"");
+} 
+}
+}
