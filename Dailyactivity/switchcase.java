3267c198f8243199f1f7c6151b272ef7df5659c4
Deepavijayakumar99 <103984788+Deepavijayakumar99@users.noreply.github.com>
2022-05-01 14:58:10 +0530

    Add files via upload

---
+++ Dailyactivity/switchcase.java
@@ -0,0 +1,18 @@
+class switchcase
+{
+public static void main(String args[])
+{
+int number=10;
+switch(number)
+{
+case 1:System.out.println("number one");
+       break;
+case 2:System.out.println("number 2");
+       break;   
+case 3:System.out.println("number 3");
+       break;
+default:
+       System.out.println("invalid number..");
+}
+}
+}
