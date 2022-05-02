67793927169521f683e8f168b0869b3997b2b10a
Deepavijayakumar99 <103984788+Deepavijayakumar99@users.noreply.github.com>
2022-05-02 11:37:49 +0530

    Add files via upload

---
+++ Dailyactivity/practice.java
@@ -0,0 +1,16 @@
+class practice
+{
+static int a=10;
+practice()
+{
+a++;
+System.out.println(a);
+}
+public static void main(String args[])
+{
+practice p=new practice();
+practice p1=new practice();
+practice p2=new practice();
+practice p3=new practice();
+}
+}
