a08309b9fefff744fad13061865973d42a6db81e
Deepavijayakumar99 <103984788+Deepavijayakumar99@users.noreply.github.com>
2022-05-11 11:08:27 +0530

    Add files via upload

---
+++ Dailyactivity/arrayelement.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class arrayelement
+{
+public static void main(String args[])
+{
+int a[][]={{10,20},{40,60},{90,70}};
+System.out.println("the elements are:");
+for(int i=0;i<3;i++)
+{
+for(int j=0;j<2;j++)
+{
+System.out.println(a[i][j]);
+}
+}
+}
+}
