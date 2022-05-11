276efb6713ff178cfcd0ffa59668cfaa551fc7ed
Deepavijayakumar99 <103984788+Deepavijayakumar99@users.noreply.github.com>
2022-05-11 11:01:44 +0530

    Add files via upload

---
+++ Dailyactivity/multisizearray.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class multisizearray
+{
+public static void main(Sring args[])
+{
+int a[][]={{10,20,30},{40,60,70}};
+System.out.println(a[0][0]);
+System.out.println(a[0][1]);
+System.out.println(a[1][0]);
+System.out.println(a[1][1]);
+}
+}
