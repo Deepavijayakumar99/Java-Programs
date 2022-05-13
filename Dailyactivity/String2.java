9d5b80a1303bfa3d17d308e2b28da55b8400eefc
Deepavijayakumar99 <103984788+Deepavijayakumar99@users.noreply.github.com>
2022-05-13 12:37:25 +0530

    Add files via upload

---
+++ Dailyactivity/String2.java
@@ -0,0 +1,13 @@
+import java.lang.String;
+class String2
+{
+public static void main(String args[])
+{
+String a="hello";
+String b="preetha";
+System.out.println("first string is:"+a);
+System.out.println("second string is:"+b);
+String c=a.concat(b);
+System.out.println("reverse string is:"+c);
+}
+}
