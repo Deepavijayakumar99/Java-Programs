ec19c7d5de2e215b6e5a0ae88042ab27e0325a1a
Deepavijayakumar99 <103984788+Deepavijayakumar99@users.noreply.github.com>
2022-05-13 12:37:00 +0530

    Add files via upload

---
+++ Dailyactivity/String3.java
@@ -0,0 +1,11 @@
+import java.lang.String;
+class String3
+{
+public static void main(String args[])
+{
+String a="hai";
+String b="mam";
+boolean c=a.equals(b);
+System.out.println("first String is:"+c);
+}
+}
