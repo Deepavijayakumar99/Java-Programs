c1f235741e8495b9eeab683c431b36b134dcc9fa
Deepavijayakumar99 <103984788+Deepavijayakumar99@users.noreply.github.com>
2022-05-20 12:43:13 +0530

    Add files via upload

---
+++ Dailyactivity/Referencevarfinal.java
@@ -0,0 +1,13 @@
+public class Referencevarfinal{
+	void disp(int a)
+	{
+		a+=3;
+		System.out.println(a);
+	}
+
+	public static void main(String[] args) {
+		Referencevarfinal r=new Referencevarfinal();
+		r.disp(7);
+	}
+
+}
