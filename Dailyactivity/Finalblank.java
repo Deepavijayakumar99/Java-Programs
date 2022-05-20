15a3fc58852d16d160442eda855b9be9d708fe15
Deepavijayakumar99 <103984788+Deepavijayakumar99@users.noreply.github.com>
2022-05-20 12:28:26 +0530

    Add files via upload

---
+++ Dailyactivity/Finalblank.java
@@ -0,0 +1,20 @@
+public class Finalblank {
+	final int a;
+	Finalblank(int b)
+	{
+		a=b;
+	}
+	void disp()
+	{
+		
+		System.out.println(a);
+	}
+
+	public static void main(String[] args) {
+		Finalblank f=new Finalblank(2);
+		f.disp();
+		
+
+	}
+
+}
