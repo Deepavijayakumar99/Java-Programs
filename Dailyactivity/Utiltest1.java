0612ebfab57f75ab197cbe04a0c1ebd40e927e71
Deepavijayakumar99 <103984788+Deepavijayakumar99@users.noreply.github.com>
2022-05-25 12:45:09 +0530

    Add files via upload

---
+++ Dailyactivity/Utiltest1.java
@@ -0,0 +1,14 @@
+
+import java.util.Arrays;
+
+public class Utiltest1 {
+
+	public static void main(String[] args) 
+	{
+		int b[]= {10,6,70};
+		Arrays.sort(b);
+		System.out.println("elements are:"+Arrays.toString(b));
+		
+	}
+
+}
