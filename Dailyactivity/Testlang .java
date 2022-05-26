e3d8f543bcd9c5b7c8f0f9ec933bb640fe68a6ea
Deepavijayakumar99 <103984788+Deepavijayakumar99@users.noreply.github.com>
2022-05-26 09:53:33 +0530

    Add files via upload

---
+++ Dailyactivity/Testlang .java
@@ -0,0 +1,12 @@
+public class Testlang {
+
+	public static void main(String[] args)
+	{
+		int a=20,b=30;
+		int max=Math.max(a, b);
+		System.out.println("the maximum value is:"+max);
+	
+
+	}
+
+}
