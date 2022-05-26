5e4b62305e46528ebbdb3dbe022d57492dcba1aa
Deepavijayakumar99 <103984788+Deepavijayakumar99@users.noreply.github.com>
2022-05-26 11:29:12 +0530

    Add files via upload

---
+++ Testawt.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+public class Testawt
+{
+public static void main(String args[])
+{
+Frame f=new Frame();
+Button b=new Button();
+f.add(b);
+f.setvisible(true);
+b.setsize(120,450);
+f.setsize(340,230);
+}
+}
