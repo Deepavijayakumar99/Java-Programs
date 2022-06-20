88f5d3e24af260d05406805434aae6cff31a2ca5
Administrator <Administrator@PG00Y1X6>
2022-06-20 10:17:40 +0530

    commit

---
+++ sortarray_integer/src/executor/Sort_arrayelement.java
@@ -0,0 +1,26 @@
+package executor;
+
+import java.util.Arrays;
+
+public class Sort_arrayelement {
+
+	public static void main(String[] args) {
+		 
+		
+			//array define integer type 
+			int[] array=new int[] {10,8,5,3,2,7,4,1,9};
+			//sorting method
+			Arrays.sort(array);
+			System.out.println("Enter the integer into ascending order");
+			//print the array 
+			for(int i=0;i<array.length;i++)
+			{
+				System.out.println(array[i]);
+			}
+
+		}
+
+	
+	}
+
+
---
+++ sortarray_integer/src/executor/Sort_arraylist.java
@@ -0,0 +1,33 @@
+package executor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Sort_arraylist {
+
+	public static void main(String[] args) {
+		
+	        // Define an objects of ArrayList class
+	        ArrayList<Integer> list = new ArrayList<Integer>();
+	 
+	        // Adding elements to the ArrayList
+	        list.add(10);
+	        list.add(50);
+	        list.add(44);
+	        list.add(97);
+	        list.add(29);
+	        list.add(15);
+	 
+	        // Printing the unsorted ArrayList
+	        System.out.println("Before Sorting : " + list);
+	 
+	        // Sorting ArrayList in descending Order
+	        Collections.sort(list, Collections.reverseOrder());
+	 
+	        // Printing the sorted ArrayList
+	        System.out.println("After Sorting : " + list);
+	    }
+	
+	}
+
+
